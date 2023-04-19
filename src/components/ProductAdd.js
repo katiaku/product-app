c0021a4379427c122332d478b8017e02704f5cdc
Katia <kushnir.ekaterina@gmail.com>
2023-04-19 23:22:09 +0200

    Create components

---
+++ src/components/ProductAdd.js
@@ -0,0 +1,192 @@
+import React, { useState } from 'react';
+import { Link,  useNavigate } from 'react-router-dom';
+import axios from 'axios'
+
+function ProductAdd() {
+
+    const [sku, setSku] = useState('');
+    const [productName, setProductName] = useState('');
+    const [price, setPrice] = useState('');
+    const [productType, setProductType] = useState('');
+    const [size, setSize] = useState('');
+    const [weight, setWeight] = useState('');
+    const [height, setHeight] = useState('');
+    const [width, setWidth] = useState('');
+    const [length, setLength] = useState('');
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const [skuError, setSkuError] = useState('');
+
+    const PRODUCT_TYPES = {
+        DVD: (
+            <label>
+                Size (MB):
+                <input
+                    id="size"
+                    type="text"
+                    value={size}
+                    onChange={(event) => setSize(event.target.value)}
+                    required
+                />
+                <span>Please, provide size</span>
+            </label>
+        ),
+        Furniture: (
+            <>
+                <label>
+                    Height (CM):
+                    <input
+                        id="height"
+                        type="text"
+                        value={height}
+                        onChange={(event) => setHeight(event.target.value)}
+                        required
+                    />
+                </label>
+                <label>
+                    Width (CM):
+                    <input
+                        id="width"
+                        type="text"
+                        value={width}
+                        onChange={(event) => setWidth(event.target.value)}
+                        required
+                    />
+                </label>
+                <label>
+                    Length (CM):
+                    <input
+                        id="length"
+                        type="text"
+                        value={length}
+                        onChange={(event) => setLength(event.target.value)}
+                        required
+                    />
+                </label>
+                <span>Please, provide dimensions</span>
+            </>
+        ),
+        Book: (
+            <label>
+                Weight (KG):
+                <input
+                    id="weight"
+                    type="text"
+                    value={weight}
+                    onChange={(event) => setWeight(event.target.value)}
+                    required
+                />
+                <span>Please, provide weight</span>
+            </label>
+        )
+    };
+
+    const productTypeSpecificAttribute = PRODUCT_TYPES[productType];
+
+    let navigate = useNavigate();
+
+    function handleTypeSwitcher(event) {
+        setProductType(event.target.value);
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        if (!sku || !productName || !price || !productType || !productTypeSpecificAttribute) {
+            setErrorMessage('Please, submit required data');
+            return;
+        }
+
+        if (typeof sku !== 'string' || typeof productName !== 'string' || typeof price !== 'number') {
+            alert('Please, provide the data of indicated type');
+            return;
+        }
+
+        try {
+            const response = await axios.get('/api/check-sku.php?sku=' + sku);
+            if (response.data.exists) {
+                setSkuError('SKU already exists');
+            return;
+            } else {
+                setSkuError('');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
+        try {
+            await axios.post('/api/add-product.php', {
+                sku,
+                productName,
+                price,
+                productType,
+                productTypeSpecificAttribute
+            });
+            navigate('/');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <div>
+            <header>
+                <h1>Product Add</h1>
+                <Link to="/">
+                    <button>Save</button>
+                </Link>
+                <Link to="/">
+                    <button>Cancel</button>
+                </Link>
+            </header>
+            <hr></hr>
+            <form id='product_form' onSubmit={handleSubmit}>
+                <label>
+                    SKU
+                    <input
+                        id="sku"
+                        type="text"
+                        value={sku}
+                        onChange={(event) => setSku(event.target.value)}
+                        required
+                    />
+                </label>
+                <label>
+                    Name
+                    <input
+                        id="name"
+                        type="text"
+                        value={productName}
+                        onChange={(event) => setProductName(event.target.value)}
+                        required
+                    />
+                </label>
+                <label>
+                    Price ($)
+                    <input
+                        id="price"
+                        type="text"
+                        value={price}
+                        onChange={(event) => setPrice(event.target.value)}
+                        required
+                    />
+                </label>
+                <label>
+                    Type Switcher
+                    <select id="productType" value={productType} onChange={handleTypeSwitcher}>
+                        <option value="" disabled selected hidden>Type Switcher</option>
+                        <option value="DVD">DVD</option>
+                        <option value="Furniture">Furniture</option>
+                        <option value="Book">Book</option>
+                    </select>
+                </label>
+                {productTypeSpecificAttribute}
+            </form>
+            <footer>
+                <p>Scandiweb Test assignment</p>
+            </footer>
+        </div>
+    )
+}
+
+export default ProductAdd;
---
+++ src/components/ProductList.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios'
+
+function ProductList() {
+
+    const [productList, setProductList] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/list-products.php')
+            .then(response => {
+                setProductList(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
+    const handleCheckbox = (event, productId) => {
+        const updatedProductList = productList.map(product => {
+            if (product.id === productId) {
+                return {
+                    ...product,
+                    checked: event.target.checked
+                }
+            }
+            return product;
+        });
+        setProductList(updatedProductList)
+    }
+
+    const massDelete = () => {
+        const selectedProducts = productList.filter(product => product.checked).map(product => product.id);
+        axios.post('/api/delete-products.php', { ids: selectedProducts })
+        .then(response => {
+            setProductList(productList.filter(product => !product.checked))
+        })
+        .catch(error => {
+            console.log(error)
+        });
+    }
+
+    return (
+        <div>
+            <header>
+                <h1>Product List</h1>
+                <Link to="/product-add">
+                    <button>ADD</button>
+                </Link>
+                <button onClick={massDelete}>MASS DELETE</button>
+            </header>
+            <hr></hr>
+            <section>
+                {productList.map(product => {
+                    return (
+                        <div key={product.id}>
+                            <input type="checkbox" className="delete-checkbox" checked={product.checked} onChange={event => handleCheckbox(event, product.id)}></input>
+                            <p>{product.getSku()}</p>
+                            <p>{product.getProductName()}</p>
+                            <p>{product.getPrice()}</p>
+                            <p>{product.getProductTypeSpecificAttribute()}</p>
+                        </div>
+                    )
+                })}
+            </section>
+            <footer>
+                <p>Scandiweb Test assignment</p>
+            </footer>
+        </div>
+    )
+}
+
+export default ProductList;
