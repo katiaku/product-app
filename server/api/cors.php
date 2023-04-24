42b34c061a525f24ffe69e5d9a070b1899b6a31f
Katia <kushnir.ekaterina@gmail.com>
2023-04-24 18:31:37 +0200

    Change access control allow methods

--- server/api/cors.php
+++ server/api/cors.php
@@ -7,7 +7,7 @@ if (isset($_SERVER['HTTP_ORIGIN'])
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header('Access-Control-Allow-Methods: GET, POST, DELETE');
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE');
     header('Access-Control-Allow-Headers: *');
     exit(0);
 }
