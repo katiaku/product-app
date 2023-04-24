e4fb7d252e91c99c44b9a4036e60c51fcbd33681
Katia <kushnir.ekaterina@gmail.com>
2023-04-24 16:27:51 +0200

    Update headers

--- server/api/cors.php
+++ server/api/cors.php
@@ -9,6 +9,6 @@ if (isset($_SERVER['HTTP_ORIGIN'])
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     header('Access-Control-Allow-Methods: GET, POST, DELETE');
-    header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+    header('Access-Control-Allow-Headers: *');
     exit(0);
 }
