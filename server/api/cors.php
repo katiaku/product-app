ea0a85eb0553011946dad0aaa78898a750805244
Katia <kushnir.ekaterina@gmail.com>
2023-04-24 15:43:06 +0200

    Update cors

--- server/api/cors.php
+++ server/api/cors.php
@@ -1,7 +1,8 @@
 <?php
 
 
-if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] === 'http://localhost:3000') {
+if (isset($_SERVER['HTTP_ORIGIN']) 
+&& $_SERVER['HTTP_ORIGIN'] === 'http://localhost:3000') {
     header('Access-Control-Allow-Origin: http://localhost:3000');
     header('Access-Control-Max-Age: 86400');
 }
