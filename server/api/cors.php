89291ced677b68296dc74a0fc9658075c669e64d
Katia <kushnir.ekaterina@gmail.com>
2023-04-24 16:17:49 +0200

    Update cors headers

--- server/api/cors.php
+++ server/api/cors.php
@@ -2,13 +2,13 @@
 
 
 if (isset($_SERVER['HTTP_ORIGIN']) 
-&& $_SERVER['HTTP_ORIGIN'] === 'http://localhost:3000') {
-    header('Access-Control-Allow-Origin: http://localhost:3000');
+&& $_SERVER['HTTP_ORIGIN'] === '*') {
+    header('Access-Control-Allow-Origin: *');
     header('Access-Control-Max-Age: 86400');
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     header('Access-Control-Allow-Methods: GET, POST, DELETE');
-    header('Access-Control-Allow-Headers: Content-Type');
+    header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
     exit(0);
 }
